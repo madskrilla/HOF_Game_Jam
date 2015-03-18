8ee5af8e5d3933929da7cca00833331d48e1b36d
Richard Lizana <rwlizana89@fullsail.edu>
2015-03-18 12:21:23 -0400

    added Track Class

---
+++ ConsoleApplication1/ConsoleApplication1/Track_Elements/Track.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Otter;
+
+namespace ConsoleApplication1.Track_Elements
+{
+    class Track : Entity
+    {
+        public Track() : base()
+        {
+
+        }
+    }
+}
