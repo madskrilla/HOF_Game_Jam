bb3095212b5b3578a4406756a8d5ca1d2f36094a
Conner McLaine <CMcLaine92>
2015-03-18 21:53:18 -0400

    Unversioned Files added

---
+++ ConsoleApplication1/ConsoleApplication1/Items/Rocket.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Otter;
+using ConsoleApplication1.Vehicles;
+using ConsoleApplication1.Scenes;
+
+
+
+
+namespace ConsoleApplication1.Items
+{
+    
+    class Rocket : PickUp
+    {
+       
+        public Vector2 velocity;
+        public int speed = 10;
+       
+
+        public Rocket(Slot_Car _owner, Race race) : base(_owner, race)
+        {
+            owner = _owner;
+            theRace = race;
+            this.itemCollider.Collidable = false;
+            this.itemImage.Visible = false;
+        }
+
+        public override void Execute()
+        {
+            this.itemCollider.Collidable = true;
+            this.itemImage.Visible = true;
+            velocity = owner.velocity;
+            velocity.Normalize();
+            velocity *= speed;
+        }
+
+        public override void Update()
+        {
+            X += velocity.X;
+            Y += velocity.Y; 
+        }
+    
+
+
+    }
+
+
+
+
+}
+
